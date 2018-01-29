fc2a787b038f0ed8a280089bc3c4c9880f710649
drywalt <35910035+drywalt@users.noreply.github.com>
2018-01-28 23:33:54 -0500

    Create MoreVariables.java

---
+++ MoreVariables.java
@@ -0,0 +1,22 @@
+package week2;
+
+public class MoreVariables {
+
+	public static void main(String[] args) {
+		char letterGrade;
+		int numericGrade;
+		String course;
+		
+		//assignments
+		letterGrade = 'B';
+		numericGrade = 87;
+		course = "Computer Science I";
+		
+		System.out.println("Your grade in " + course +
+							" is " + numericGrade +
+							" which is a " + letterGrade);
+		
+
+	}
+
+} 
